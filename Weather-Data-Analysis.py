e8379b7af09273cc240c6e9afadd8d9984509ea4
unknown <abdelhakim@univ-tiaret.dz>
2020-12-03 18:22:55 +0100

    data count

--- Weather-Data-Analysis.py
+++ Weather-Data-Analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 data = pd.read_csv('C:/Users/PC/Desktop/Weather-Data-Analysis/Weather-Data.csv')
 print(data)
 print(data.head())
@@ -8,3 +9,4 @@ print(data.columns)
 print(data.dtypes)
 print(data['Weather'].unique())
 print(data.nunique())
+print(data.count())
